7c3b58f9b1c27267c597dd95314f5e4b42edb7ef
Harsh91736 <harshdhameliya06@gmail.com>
2024-07-30 09:56:29 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+
+// alert will give the notification of written text
+
+alert("hellow");
+
+// consol.log is use to debug the program that where the program is rich
+
+console.log("the code is running");
+console.log("the code is still  running");
+console.log("the code is  in not running");
+
+// prompt is use take input in js
+
+var a=prompt("enter your number")
+
+console.log("your number is " + a)
+
+// confirm will take sujestion in form ok or cancle if ok then istrue will assign 1 or 0
+
+var istrue=confirm("are you sure you want to blast your computer")
+
+if(istrue){
+    console.log("your computer is blasted")
+
+}
+else{
+    console.log("your computer is safe")
+}
+
